302cc4077c9e262d2072c434225082c6450e9525
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-05-02 23:22:29 +0200

    fix:filter

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -70,6 +70,7 @@ export default function CardList({
               cardID={movie.id}
               movie={movie}
               setModal={setModal}
+              needActions={true}
             />
           )
         )
