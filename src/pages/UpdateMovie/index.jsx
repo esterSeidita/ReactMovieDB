9d8351fc3177821046ff572e055274fec5b43620
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-05-04 16:58:50 +0200

    fix

--- src/pages/UpdateMovie/index.jsx
+++ src/pages/UpdateMovie/index.jsx
@@ -1,16 +1,13 @@
 import styles from "./style.module.scss";
-import { GetData, UpdateData } from "../../utils";
+import { GetData } from "../../utils";
 import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import Form from "../../components/Form";
-import {useNavigate  } from "react-router-dom";
 
 export default function UpdateMovie({ setAlertData, updateCardId }) {
   const [inputs, setInputs] = useState({});
   const [cardData, setCardData] = useState([]);
   const [showModal, setShowModal] = useState("not-visible");
-
-  const navigate = useNavigate();
 
   useEffect(() => {
     GetData(updateCardId).then((data)=>
